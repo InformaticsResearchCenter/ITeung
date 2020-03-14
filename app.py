3669cd5d6d82fcd37464ee7e8983a5c95f581eef
Rolly Maulana Awangga <rolly@awang.ga>
2020-03-14 11:32:38 +0700

    ok

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Created on Sat Mar 14 09:27:37 2020
 @author: rolly
 """
 
-from flask import Flask
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
@@ -13,11 +13,12 @@ app = Flask(__name__)
 @app.route("/wa", methods=['GET', 'POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
+    body = request.values.get('Body', None)
     # Start our TwiML response
     resp = MessagingResponse()
 
     # Add a message
-    resp.message("The Robots are coming! Head for the crooot!")
+    resp.message(body)
     #resp.media("https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg")
 
     return str(resp)
