b022d442173b92be2bb68cbf7f45e05b9642f2e0
Rolly Maulana Awangga <rolly@awang.ga>
2020-03-14 11:29:12 +0700

    ok

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def sms_reply():
 
     # Add a message
     resp.message("The Robots are coming! Head for the crooot!")
-    resp.media("https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg")
+    #resp.media("https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg")
 
     return str(resp)
 
