671e85015d54f149c86f199e18f21510997a7ccf
DindaMajesty <dindamesti@gmail.com>
2020-03-16 12:09:38 +0700

    kelas mulai

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 from lib import wa,reply,message
 import config
 from importlib import import_module
+from datetime import datetime
 
 
 
@@ -10,14 +11,12 @@ class Chatbot(object):
         wa.loginWA(driver)
         while True:
             self.cekAndSendMessage(driver)
-        
-       
+
+
     def cekAndSendMessage(self,driver):
         try:
             wa.openMessage(driver)
             data = wa.getData(driver)
-
-            print(data)
 
             if len(data) == 3:
                 msg = data[2]
@@ -36,25 +35,69 @@ class Chatbot(object):
                 else:
                     msgreply=reply.message(msg)
                     if msgreply[:2] == 'm:':
-                        group_id = reply.getNumberGroup(num)
-                        if group_id is not None:
-                            status = reply.getAuth(group_id, msgreply)
-                        else:
-                            status=False
-                        if status == True:
+                        if msgreply[2:] == 'prodi':
+                            group_id = reply.getNumberGroup(num)
+                            if group_id is not None:
+                                status = reply.getAuth(group_id, msgreply)
+                            else:
+                                status=False
+                            if status == True:
+                                modulename = msgreply.split(':')[1]
+                                mod=import_module('module.'+modulename)
+                                wmsg=reply.getWaitingMessage(modulename)
+                                wa.typeAndSendMessage(driver,wmsg)
+                                msgreply=mod.reply(msg)
+                            else:
+                                msgreply=reply.getReplyAuth()
+                        if msgreply[2:] == 'kelas':
                             modulename = msgreply.split(':')[1]
-                            mod=import_module('module.'+modulename)
-                            wmsg=reply.getWaitingMessage(modulename)
-                            wa.typeAndSendMessage(driver,wmsg)
-                            msgreply=mod.reply(msg)
-                        else:
-                            msgreply=reply.getReplyAuth()
+                            mod= import_module('module.'+modulename)
+                            if mod.numberAuth(num):
+                                wmsg=reply.getContentWithKeyword(modulename)
+                                msgreply=wmsg.replace("#MATKUL#", mod.mulaiMatkul(msg))
+                                wa.typeAndSendMessage(driver, msgreply)
+                                kelas = True
+                                msgcek=''
+                            else:
+                                msgreply="Enak aja main mulai kelas tunggu dosennya dulu dong :-)"
+
+                            while kelas:
+                                data=wa.getData(driver)
+
+                                getDateTimeNow=datetime.now()
+
+                                datetimenow=getDateTimeNow.strftime("%d/%m/%Y %H:%M:%S")
+
+                                msg = data[2]
+                                als = data[1]
+                                num = data[0]
+
+                                msg = message.normalize(msg)
+                                msgs = msg.split(" ")
+
+                                if msgcek != msg:
+                                    
+                                    msgcek = msg
+
+                                if msg.find(config.bot_name) >= 0:
+                                    if len(msgs) == 1:
+                                        msgreply = reply.getOpeningMessage()
+                                    else:
+                                        msgreply = reply.message(msg)
+                                        if msgreply[:2] == 'm:':
+                                            if msgreply[2:] == 'kelas':
+                                                if msgs[-1] == "selesai":
+                                                    modulename = msgreply.split(":")[1]
+                                                    mod=import_module('module.'+modulename)
+                                                    namamatkul=mod.selesaiMatkul(msg)
+                                                    msgreply="oke, matkul "+namamatkul+" selesai"
+                                                    kelas=False
+
+
         except Exception as e:
             print(e)
             msgreply=reply.getErrorMessage()
             msgreply=msgreply.replace("#ERROR#", str(e))
-
-        print(locals())
 
         if 'msgreply' in locals():
             if msgreply[:2] != 'm:':
