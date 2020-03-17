2b6b1c6af8a10ac1fff29fd351b8ae6559e7e00a
DindaMajesty <dindamesti@gmail.com>
2020-03-17 17:45:51 +0700

    restucture

--- lib/iteung.py
+++ lib/iteung.py
@@ -4,7 +4,7 @@ Created on Sat Mar 14 12:36:17 2020
 
 @author: rolly
 """
-from lib import reply,message,twl,auth
+from lib import reply,message,twl
 import config
 from importlib import import_module
 
@@ -12,21 +12,24 @@ def get(num,msg):
     try:
         msg  = message.normalize(msg)
         msgs = list(msg.split(" "))
+
         if len(msgs) == 1:
             msgreply = reply.getOpeningMessage()
         else:
             msgreply=reply.message(msg)
             if msgreply[:2] == 'm:':
                 modulename = msgreply.split(':')[1]
-                if auth.valid(num, msgreply):
-                    mod=import_module('module.'+modulename)
-                    msgreply=mod.replymsg(msg)
-                else:
-                    msgreply=reply.getReplyAuth(modulename)
+                mod=import_module('module.'+modulename)
+                msgreply=mod.replymsg(msg)
+            else:
+                modulename = msgreply.split(':')[1]
+                msgreply=reply.getReplyAuth(modulename)
     except Exception as e:
         print(e)
         msgreply=reply.getErrorMessage()
         msgreply=msgreply.replace("#ERROR#", str(e))
+
+    print(locals())
 
     if 'msgreply' in locals():
         if msgreply[:2] != 'm:':
