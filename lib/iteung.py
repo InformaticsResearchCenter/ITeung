4effdb4cdb544d698489d07ee76b4bb69ed084fc
DindaMajesty <dindamesti@gmail.com>
2020-03-17 16:32:34 +0700

    iteung twilio

--- lib/iteung.py
+++ lib/iteung.py
@@ -4,7 +4,7 @@ Created on Sat Mar 14 12:36:17 2020
 
 @author: rolly
 """
-from lib import reply,message,twl
+from lib import reply,message,twl,auth
 import config
 from importlib import import_module
 
@@ -12,31 +12,21 @@ def get(num,msg):
     try:
         msg  = message.normalize(msg)
         msgs = list(msg.split(" "))
-
         if len(msgs) == 1:
             msgreply = reply.getOpeningMessage()
         else:
             msgreply=reply.message(msg)
             if msgreply[:2] == 'm:':
-                group_id = reply.getNumberGroup(num)
-                if group_id is not None:
-                    status = reply.getAuth(group_id, msgreply)
-                else:
-                    status=False
-                if status == True:
-                    modulename = msgreply.split(':')[1]
+                modulename = msgreply.split(':')[1]
+                if auth.valid(num, msgreply):
                     mod=import_module('module.'+modulename)
-                    wmsg=reply.getWaitingMessage(modulename)
-                    twl.sendMsg(num,wmsg)
-                    msgreply=mod.reply(msg)
+                    msgreply=mod.replymsg(msg)
                 else:
-                    msgreply=reply.getReplyAuth()
+                    msgreply=reply.getReplyAuth(modulename)
     except Exception as e:
         print(e)
         msgreply=reply.getErrorMessage()
         msgreply=msgreply.replace("#ERROR#", str(e))
-
-    print(locals())
 
     if 'msgreply' in locals():
         if msgreply[:2] != 'm:':
