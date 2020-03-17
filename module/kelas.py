12202d07ad4867ffca8fab1bee6e409a0670d865
DindaMajesty <dindamesti@gmail.com>
2020-03-17 13:09:00 +0700

    siap absensi

--- module/kelas.py
+++ module/kelas.py
@@ -1,6 +1,8 @@
 from lib import lecturerPhoneNumber, message, wa, reply
 from datetime import datetime
 from importlib import import_module
+from time import sleep
+
 import config
 import pymysql
 
@@ -69,6 +71,8 @@ def startkelas(driver, kelas, msg):
     alscek=''
     numcek=''
 
+    print(getNamaGroup(driver))
+
     discussion=getDiscussion(msg)
     course=getMatkul(msg)
     kodedosen=getKodeDosen(msg)
@@ -109,9 +113,10 @@ def startkelas(driver, kelas, msg):
                             modulename = msgreply.split(":")[1]
                             mod = import_module('module.' + modulename)
                             namamatkul = mod.selesaiMatkul(msg)
-                            msgreply = "oke, matkul " + namamatkul + " selesai"
+                            msgreply = "oke, matkul " + namamatkul + " selesai, dan teman teman yang hadir tadi sudah diinputkan ke absensi siap, terima kasih Bapak/Ibu dosen yang sudah mengajar, selamat beraktivitas kembali, dan jangan lupa ya Cuci Tangan agar temen-temen semua sehat, sampai ketemu dipertemuan selanjutnya dadahhhhh........"
                             kelas = False
     beritaAcara(driver, kodedosen, course, discussion, timestart)
+    siapAbsensi(driver, kodedosen, getNamaGroup(driver), timestart)
     return msgreply
 
 def getAwaitingMessageKelasStart(module):
@@ -174,6 +179,9 @@ def getNamaDosen(kodedosen):
             nama=rows[0]
     return nama
 
+def getNamaGroup(driver):
+    return driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/header/div[2]/div[1]/div/span').text
+
 def getHadirNpm(time):
     db = dbConnect()
     sql="SELECT DISTINCT npm from d4ti_3a WHERE date_time > '{0}'".format(time)
@@ -206,3 +214,122 @@ def beritaAcara(driver, kodedosen, course, discussion, timestart):
     for npm in data:
         wa.typeAndSendMessage(driver, str(number)+". "+npm)
         number=int(number)+1
+
+def siapAbsensi(driver, kodedosen, namagroup, timestart):
+    kehadirannpm = getHadirNpm(timestart)
+    datakehadirannpm = []
+    for kehadiran in kehadirannpm:
+        for npm in kehadiran:
+            datakehadirannpm.append(npm)
+    namagroupsplit=namagroup.split("-")
+    kodematkul=namagroupsplit[0]
+    kodekelas=namagroupsplit[1]
+    openSiapwithNewTab(driver)
+    switchWindowsHandleto1(driver)
+    try:
+        loginSiap(driver)
+    except:
+        print("sudah login")
+    sleep(1)
+    ClickPresensi(driver)
+    TahunAkad(driver)
+    findLecturerCode(driver, kodedosen)
+    findMatkul(driver, kodematkul, kodekelas)
+    TambahPresensi(driver)
+    simpan(driver)
+    sleep(1)
+    AddMahasiswa(driver)
+    Mahasiswa(driver, datakehadirannpm)
+    Refresh(driver)
+    closeTab(driver)
+    switchWindowsHandleto0(driver)
+
+def closeTab(driver):
+    return driver.close()
+
+def openSiapwithNewTab(driver):
+    return driver.execute_script("window.open('http://siap.poltekpos.ac.id/siap/besan.depan.php');")
+
+def switchWindowsHandleto1(driver):
+    return driver.switch_to_window(driver.window_handles[1])
+
+def switchWindowsHandleto0(driver):
+    return driver.switch_to_window(driver.window_handles[0])
+
+def loginSiap(driver):
+    username = driver.find_elements_by_class_name("textbox")[0]
+    username.send_keys(config.username_siap)
+
+    sleep(1)
+
+    password = driver.find_elements_by_class_name("textbox")[1]
+    password.send_keys(config.password_siap)
+
+    sleep(1)
+
+    login = driver.find_element_by_class_name("button")
+    login.click()
+
+def ClickPresensi(driver):
+    return driver.find_elements_by_class_name("side")[-1].click()
+
+def findLecturerCode(driver, kodedosen):
+    lecturercode = driver.find_elements_by_xpath('/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p/table/tbody/tr[2]/td[2]/select/option')
+    for i in lecturercode:
+        kodesplit = i.text.split(" ")
+        codeoflecturer = kodesplit[-1]
+        codeoflecturer = codeoflecturer.lower()
+        if kodedosen == codeoflecturer:
+            i.click()
+            break
+
+def findMatkul(driver, matkul, kelas):
+    print(matkul)
+    print(kelas)
+    listofmatkul=driver.find_elements_by_xpath('/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p/table/tbody/tr[3]/td[2]/select/option')
+    for data in listofmatkul:
+        matkulsplit=data.text.split(" ")
+        if matkulsplit[-1] == '---':
+            kodematkul=''
+            kodekelas=''
+        else:
+            kodematkul=matkulsplit[2]
+            kodekelas=matkulsplit[-6]
+        if kodematkul == matkul and kodekelas == kelas:
+            data.click()
+            break
+
+def TambahPresensi(driver):
+    return driver.find_element_by_xpath(
+        "/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[2]/table/tbody/tr[5]/td/input[2]").click()
+
+def simpan(driver):
+    return driver.find_element_by_xpath(
+        "/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[3]/table/tbody/tr[7]/td/input[1]").click()
+
+def AddMahasiswa(driver):
+    jumTabel = int(driver.find_elements_by_class_name("inp1")[-1].text)
+    return driver.find_element_by_xpath("/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[3]/table/tbody/tr[" + str(jumTabel + 1) + "]/td[8]/a").click()
+
+
+def Mahasiswa(driver, npm):
+    mahasiswa = npm
+    jumMahasiswa = int(driver.find_elements_by_class_name("inp1")[-1].text)
+    index = 1
+    for getNpm in range(jumMahasiswa):
+        npm = driver.find_element_by_xpath("/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[4]/table/tbody/tr[" + str(
+                index) + "]/td[2]").text
+        for npmmahasiswa in mahasiswa:
+            if npmmahasiswa == npm:
+                driver.find_element_by_xpath(
+                    "/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[4]/table/tbody/tr[" + str(
+                        index) + "]/td[4]/select/option[1]").click()
+        index += 1
+
+
+def Refresh(driver):
+    driver.find_element_by_xpath(
+        "/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[2]/table/tbody/tr[5]/td/input[3]").click()
+
+def TahunAkad(driver):
+    return driver.find_element_by_xpath("/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[1]/table/tbody/tr[1]/td[3]/select/option[2]").click()
