8ed59b0060efa91c0c163db490f8e7a6504c1416
DindaMajesty <dindamesti@gmail.com>
2020-03-24 17:16:50 +0700

    log.py

---
+++ lib/log.py
@@ -0,0 +1,20 @@
+import pymysql, config
+
+from datetime import datetime
+
+def dbConnectIteung():
+    db = pymysql.connect(config.db_host, config.db_username, config.db_password, config.db_name)
+    return db
+
+def getDatetimeNow():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def save(data):
+    return inserttolog(data)
+
+def inserttolog(data):
+    db = dbConnectIteung()
+    sql = "INSERT INTO log VALUES({0}, {1}, {2}, {3}, {4}, {5})".format(getDatetimeNow(), data[0], data[3], data[2], data[1], data[4])
+    with db:
+        cur = db.cursor()
+        cur.execute(sql)
