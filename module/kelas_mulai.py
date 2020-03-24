2c0974f5c7c825e1c659653205e6aaecff9e5b43
DindaMajesty <dindamesti@gmail.com>
2020-03-24 18:51:02 +0700

    initial commit

---
+++ module/kelas_mulai.py
@@ -0,0 +1,21 @@
+from lib import wa
+from module import kelas
+
+import config
+
+
+def replymsg(driver, data):
+    grp = data[1]
+    num = data[0]
+    coursename = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num)[1]
+    if kelas.sudahinput(wa.getGroupName(driver)) == True:
+        msgreply = "mohon maaf matakuliah ini tidak bisa dimulai, mohon menunggu hingga minggu depan... terima kasih"
+    else:
+        if kelas.isMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num):
+            messages = kelas.getAwaitingMessageKelasStart('kelas')
+            messages = messages.replace('#MATKUL#', coursename)
+            messages = messages.replace('#BOTNAME#', config.bot_name)
+            msgreply = messages
+        else:
+            msgreply = 'mohon maaf kode dosen bapak/ibu dengan kode matkul ini tidak ditemukan'
+    return msgreply
