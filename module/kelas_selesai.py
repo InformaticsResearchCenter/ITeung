9eaab057232cb0e1ddd1bbbfff588f1922dca658
DindaMajesty <dindamesti@gmail.com>
2020-03-25 08:05:19 +0700

    fix parameter

--- module/kelas_selesai.py
+++ module/kelas_selesai.py
@@ -6,6 +6,6 @@ def replymsg(driver, data):
     coursename = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num)[1]
     starttimeclass = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num)[3]
     endtimeclass = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num)[4]
-    msgreply=kelas.siapAbsensi(driver=driver, namagroup=grp, num=num, namamatkul=kelas.getDataMatkul(kodematkul=grp.split('-')[0], kodekelas=grp.split('-')[1], num=num))
+    msgreply=kelas.siapAbsensi(driver=driver, namagroup=grp, num=num,)
     msgreply=kelas.beritaAcara(driver=driver, num=num, coursename=coursename, starttimeclass=starttimeclass, endtimeclass=endtimeclass, groupname=grp, data=msgreply)
     return msgreply
