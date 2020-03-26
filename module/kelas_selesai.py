f838b003d430c5076175df283ad89435c6232211
Rolly Maulana Awangga <rolly@awang.ga>
2020-03-27 00:09:41 +0700

    penangnan error ketika selesai dengna petunjuk kode

--- module/kelas_selesai.py
+++ module/kelas_selesai.py
@@ -1,6 +1,6 @@
 from module import kelas
-from lib import wa
-import config
+from lib import wa,reply
+import os
 
 def auth(data):
     if kelas.getKodeDosen(data[0]) == '':
@@ -10,14 +10,18 @@ def auth(data):
     return ret
 
 def replymsg(driver, data):
+    wmsg=reply.getWaitingMessage(os.path.basename(__file__).split('.')[0])
+    wa.typeAndSendMessage(driver,wmsg)
     grp = data[1]
     num = data[0]
-    messages = kelas.getAwaitingMessageKelasStart('kelas_selesai')
-    messages = messages.replace('#BOTNAME#', config.bot_name)
-    wa.typeMessage(driver, messages)
-    coursename = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num)[1]
-    starttimeclass = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num)[3]
-    endtimeclass = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num)[4]
-    msgreply=kelas.siapAbsensi(driver=driver, namagroup=grp, num=num,)
-    msgreply=kelas.beritaAcara(driver=driver, num=num, coursename=coursename, starttimeclass=starttimeclass, endtimeclass=endtimeclass, groupname=grp, data=msgreply)
+    try:
+        coursename = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num)[1]
+        starttimeclass = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num)[3]
+        endtimeclass = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), num)[4]
+        msgreply=kelas.siapAbsensi(driver=driver, namagroup=grp, num=num,)
+        msgreply=kelas.beritaAcara(driver=driver, num=num, coursename=coursename, starttimeclass=starttimeclass, endtimeclass=endtimeclass, groupname=grp, data=msgreply)
+    except:
+        listMK=kelas.getListMK(kelas.getKodeDosen(data[0]))
+        guide = 'yahh....nama groupnya jangan diubah dong. hihihi coba ganti lagi ya... : \n'
+        msgreply = guide+listMK
     return msgreply
