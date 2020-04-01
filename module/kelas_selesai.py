8d148965c30b5bfdcdeff3e6b806f6c468166a9f
Rolly Maulana Awangga <rolly@awang.ga>
2020-04-02 06:09:10 +0700

    debug iteung

--- module/kelas_selesai.py
+++ module/kelas_selesai.py
@@ -12,17 +12,17 @@ def auth(data):
 def replymsg(driver, data):
     grp = data[1]
     num = data[0]
-    try:
-        coursename = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), kelas.getKodeDosen(num))[1]
-        starttimeclass = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), kelas.getKodeDosen(num))[3]
-        endtimeclass = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), kelas.getKodeDosen(num))[4]
-        wmsg=reply.getWaitingMessage(os.path.basename(__file__).split('.')[0])
-        wa.typeAndSendMessage(driver,wmsg)
-        msgreply=kelas.siapabsensiwithsql(grp=grp, num=num)
-        # msgreply=kelas.siapAbsensi(driver=driver, namagroup=grp, num=num,) for website
-        msgreply=kelas.beritaAcara(driver=driver, num=num, coursename=coursename, starttimeclass=starttimeclass, endtimeclass=endtimeclass, groupname=grp, data=msgreply)
-    except:
-        listMK=kelas.getListMK(kelas.getKodeDosen(data[0]))
-        guide = 'yahh....nama groupnya jangan diubah dong. hihihi ganti lagi ya.jadi KodeMK-KELAS-NAMAMK,contoh : TI3466-A-KECERDASAN BUATAN : \n'
-        msgreply = guide+listMK
+#    try:
+    coursename = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), kelas.getKodeDosen(num))[1]
+    starttimeclass = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), kelas.getKodeDosen(num))[3]
+    endtimeclass = kelas.getDataMatkul(grp.split('-')[0], kelas.kodeKelas(grp.split('-')[1]), kelas.getKodeDosen(num))[4]
+    wmsg=reply.getWaitingMessage(os.path.basename(__file__).split('.')[0])
+    wa.typeAndSendMessage(driver,wmsg)
+    msgreply=kelas.siapabsensiwithsql(grp=grp, num=num)
+    # msgreply=kelas.siapAbsensi(driver=driver, namagroup=grp, num=num,) for website
+    msgreply=kelas.beritaAcara(driver=driver, num=num, coursename=coursename, starttimeclass=starttimeclass, endtimeclass=endtimeclass, groupname=grp, data=msgreply)
+#    except:
+#        listMK=kelas.getListMK(kelas.getKodeDosen(data[0]))
+#        guide = 'yahh....nama groupnya jangan diubah dong. hihihi ganti lagi ya.jadi KodeMK-KELAS-NAMAMK,contoh : TI3466-A-KECERDASAN BUATAN : \n'
+#        msgreply = guide+listMK
     return msgreply
