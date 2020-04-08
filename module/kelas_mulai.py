be995ed5e2fdecd5f6a5830652a4d17b0dce4e47
DindaMajesty <dindamesti@gmail.com>
2020-04-08 14:35:37 +0700

    fix if

--- module/kelas_mulai.py
+++ module/kelas_mulai.py
@@ -17,40 +17,37 @@ def auth(data):
 def replymsg(driver, data):
     grp = data[1]
     kehadiran=kelas.getKehadiran(grp.split('-')[0])
-    if  kehadiran!= config.kehadiran and kehadiran < config.kehadiran:
-        if kelas.sudahinput(wa.getGroupName(driver)) == True:
-            msgreply = "yuhuuuu..... semangat banget sih, kan minggu ini udah kuliah. tunggu minggu depan ya... terima kasih"
-        else:
-            try:
-                if kelas.isMatkul(grp.split('-')[0]):
-                    jadwalid = grp.split('-')[0]
-                    jadwalserial = kelas.getJadwalSerial(jadwalid=jadwalid)
-                    if jadwalserial == '0':
-                        jadwalid = jadwalid
-                    else:
-                        jadwalid = jadwalserial
-                    abc = 1
-                    listStudent='\n\nBerikut Peserta Absensinya:\n'
-                    for i in kelas.pesertaAbsensi(jadwalid=jadwalid):
-                        npm=i[-1]
-                        nama=kelas.getStudentNameOnly(npm)
-                        listStudent=listStudent+str(abc)+'. '+npm+' '+nama+'\n'
-                        abc+=1
-                    coursename = kelas.getDataMatkul(grp.split('-')[0])[1]
-                    messages = reply.getWaitingMessage(os.path.basename(__file__).split('.')[0])
-                    messages = messages.replace('#MATKUL#', coursename)
-                    messages = messages.replace('#BOTNAME#', config.bot_name)
-                    msgreply = messages + listStudent
+    try:
+        if kehadiran != config.kehadiran and kehadiran < config.kehadiran:
+            if kelas.isMatkul(grp.split('-')[0]):
+                jadwalid = grp.split('-')[0]
+                jadwalserial = kelas.getJadwalSerial(jadwalid=jadwalid)
+                if jadwalserial == '0':
+                    jadwalid = jadwalid
                 else:
-                    listMK=kelas.getListMK(kelas.getKodeDosen(data[0]))
-                    guide = 'Yahh... Nama grupnya belum JADWALID-KELAS-NAMA. yuk ubah #BOTNAME# kasih contoh 17312-A-KECERDASAN BUAT klo lupa kode mata kuliah #BOTNAME# kasih ya ini daftarnya : \n'
-                    msgreply = guide+listMK
-            except:
+                    jadwalid = jadwalserial
+                abc = 1
+                listStudent='\n\nBerikut Peserta Absensinya:\n'
+                for i in kelas.pesertaAbsensi(jadwalid=jadwalid):
+                    npm=i[-1]
+                    nama=kelas.getStudentNameOnly(npm)
+                    listStudent=listStudent+str(abc)+'. '+npm+' '+nama+'\n'
+                    abc+=1
+                coursename = kelas.getDataMatkul(grp.split('-')[0])[1]
+                messages = reply.getWaitingMessage(os.path.basename(__file__).split('.')[0])
+                messages = messages.replace('#MATKUL#', coursename)
+                messages = messages.replace('#BOTNAME#', config.bot_name)
+                msgreply = messages + listStudent
+            else:
                 listMK=kelas.getListMK(kelas.getKodeDosen(data[0]))
-                guide = 'Di setting dulu ya nama groupnya jadi JADWALID-KELAS-NAMA contoh : 17312-A-KECERDASAN BUAT, ini daftarnya : \n'
+                guide = 'Yahh... Nama grupnya belum JADWALID-KELAS-NAMA. yuk ubah #BOTNAME# kasih contoh 17312-A-KECERDASAN BUAT klo lupa kode mata kuliah #BOTNAME# kasih ya ini daftarnya : \n'
                 msgreply = guide+listMK
-    elif kehadiran == '':
-        msgreply='yahhhhh kehadirannya ngga #BOTNAME# temuin coba di cek lagi jadwal idnya yaaa....'
-    else:
-        msgreply='Mohon maaf Untuk kuliah daring ini sudah memenuhi kuota pertemuan pada jadwal kali ini...'
+        elif kehadiran == '':
+            msgreply = 'yahhhhh kehadirannya ngga #BOTNAME# temuin coba di cek lagi jadwal idnya yaaa....'
+        else:
+            msgreply = 'Mohon maaf Untuk kuliah daring ini sudah memenuhi kuota pertemuan pada jadwal kali ini...'
+    except:
+        listMK=kelas.getListMK(kelas.getKodeDosen(data[0]))
+        guide = 'Di setting dulu ya nama groupnya jadi JADWALID-KELAS-NAMA contoh : 17312-A-KECERDASAN BUAT, ini daftarnya : \n'
+        msgreply = guide+listMK
     return msgreply
--- module/kelas_selesai.py
+++ module/kelas_selesai.py
@@ -14,28 +14,28 @@ def replymsg(driver, data):
     num = data[0]
     msg = data[3]
     kehadiran=kelas.getKehadiran(grp.split('-')[0])
-    if kehadiran != config.kehadiran and kehadiran < config.kehadiran:
-        try:
-            wmsg=reply.getWaitingMessage(os.path.basename(__file__).split('.')[0])
-            wa.typeAndSendMessage(driver,wmsg)
-            msgreply=kelas.siapabsensiwithsql(grp=grp, num=numbers.normalize(num))
+    try:
+        if kehadiran != config.kehadiran and kehadiran < config.kehadiran:
+            wmsg = reply.getWaitingMessage(os.path.basename(__file__).split('.')[0])
+            wa.typeAndSendMessage(driver, wmsg)
+            msgreply = kelas.siapabsensiwithsql(grp=grp, num=numbers.normalize(num))
             if msgreply != '':
                 if msgreply != []:
                     # msgreply=kelas.siapabsensiwithweb(driver=driver, num=num,namagroup=grp)
                     try:
-                        msgreply=kelas.beritaAcara(driver=driver, num=num, groupname=grp, data=msgreply, msg=msg)
+                        msgreply = kelas.beritaAcara(driver=driver, num=num, groupname=grp, data=msgreply, msg=msg)
                     except:
-                        msgreply='aduh materinya ga ada nich, \ncontoh format yang bener gini ya: \niteung kelas daring selesai materi chapter 04'
+                        msgreply = 'aduh materinya ga ada nich, \ncontoh format yang bener gini ya: \niteung kelas daring selesai materi chapter 04'
                 else:
-                    msgreply='aduh mahasiswanya belum ada yang update nomer hp SIAP nich, \nKalo mau ke absen cocokin dulu dong nomer hp yang ada di SIAP dengan yang kamu pakai di WhatsApp \nIngat ya gunakan format international, contoh: 6282217401448 '
+                    msgreply = 'aduh mahasiswanya belum ada yang update nomer hp SIAP nich, \nKalo mau ke absen cocokin dulu dong nomer hp yang ada di SIAP dengan yang kamu pakai di WhatsApp \nIngat ya gunakan format international, contoh: 6282217401448 '
             else:
-                msgreply="adohhhh wadohdoh peserta absensinya kosong nih, coba ganti ke jadwal id yang satunya lagi eheeeee.... :-3"
-        except:
-            listMK=kelas.getListMK(kelas.getKodeDosen(data[0]))
-            guide = 'yahh....nama groupnya jangan diubah dong. hihihi ganti lagi ya.jadi JADWALID-KELAS-NAMAMK,contoh : 17312-A-KECERDASAN BUATAN : \n'
-            msgreply = guide+listMK
-    elif kehadiran == '':
-        msgreply='yahhhhh kehadirannya ngga #BOTNAME# temuin coba di cek lagi jadwal idnya yaaa....'
-    else:
-        msgreply = 'Mohon maaf Untuk kuliah daring ini sudah memenuhi kuota pertemuan pada jadwal kali ini...'
+                msgreply = "adohhhh wadohdoh peserta absensinya kosong nih, coba ganti ke jadwal id yang satunya lagi eheeeee.... :-3"
+        elif kehadiran == '':
+            msgreply = 'yahhhhh kehadirannya ngga #BOTNAME# temuin coba di cek lagi jadwal idnya yaaa....'
+        else:
+            msgreply = 'Mohon maaf Untuk kuliah daring ini sudah memenuhi kuota pertemuan pada jadwal kali ini...'
+    except:
+        listMK=kelas.getListMK(kelas.getKodeDosen(data[0]))
+        guide = 'yahh....nama groupnya jangan diubah dong. hihihi ganti lagi ya.jadi JADWALID-KELAS-NAMAMK,contoh : 17312-A-KECERDASAN BUATAN : \n'
+        msgreply = guide+listMK
     return msgreply
