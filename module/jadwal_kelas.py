ad164db15bf9d4c983b76ddc5998989a6b2ba9b3
DindaMajesty <dindamesti@gmail.com>
2020-04-09 11:07:22 +0700

    formate quote

--- module/jadwal_kelas.py
+++ module/jadwal_kelas.py
@@ -62,7 +62,7 @@ def replymsg(driver, data):
     wmsg = wmsg.replace('#BOTNAME#', config.bot_name)
     wa.typeAndSendMessage(driver, wmsg)
     msg = data[3]
-    msg = normalizeday(msg)
+    msg = normalizeday("{msg}").format(msg=msg)
     num = data[0]
     day = dataHari(msg)
     hariId = kodeHari(day)
