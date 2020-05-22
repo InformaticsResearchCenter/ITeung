66b83d335aab85d8433142cfcc56ce8cf3764f76
dindamajesty13 <dindamesti@gmail.com>
2020-05-22 21:45:49 +0700

    fix link bimbingan

--- module/bimbingan_mahasiswa.py
+++ module/bimbingan_mahasiswa.py
@@ -16,7 +16,7 @@ def replymsg(driver, data):
     msg=message.normalize(msg)
     tipe_bimbingan=msg.split('bimbingan ')[1].split(' topik')[0]
     studentid=kelas.getNpmandNameMahasiswa(data[0])[0]
-    topik=msg.split('topik ')[1].split(' pertemuan')[0]
+    topik=msg.split('topik ')[1].split(' pertemuan')[0].replace(' ', '%20')
     pertemuan=msg.split('pertemuan ')[1].split(' nilai')[0]
     nilai=msg.split('nilai ')[1]
     msgreply='https://api.whatsapp.com/send?phone={nomoriteung}&text=iteung%20input%20bimbingan%20{tipebimbingan}%20{npm}%0Atopik%20{topikbimbingan}%0Apertemuan%20{pertemuanke}%0Anilai%20{nilai}'.format(
