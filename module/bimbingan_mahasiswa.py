b33f3ef8755cae13d72d6e9b4675ec07a113940a
dindamajesty13 <dindamesti@gmail.com>
2020-05-28 09:20:05 +0700

    make passcode for verify

--- module/bimbingan_mahasiswa.py
+++ module/bimbingan_mahasiswa.py
@@ -21,10 +21,11 @@ def replymsg(driver, data):
     topik=msg.split('topik ')[1].split(' nilai')[0].replace(' ', '%20')
     # pertemuan=msg.split('pertemuan ')[1].split(' nilai')[0]
     nilai=msg.split('nilai ')[1]
-    datenow = datetime.date(datetime.now()).strftime('%d-%m-%Y')
-    hari = datetime.now().strftime('%A')[:6]
+    datenow = datetime.date(datetime.now()).strftime('%d%m%Y')
+    hari = datetime.now().strftime('%A')
+    hari = hariSwitcher(hari)
     obj = AES.new(config.key, AES.MODE_CBC, config.iv)
-    cp = obj.encrypt(datenow + hari)
+    cp = obj.encrypt(studentid+datenow+hari)
     passcode=cp.hex()
     msgreply='https://api.whatsapp.com/send?phone={nomoriteung}&text=iteung%20input%20bimbingan%20{tipebimbingan}%20{npm}%0Atopik%20{topikbimbingan}%0Anilai%20{nilai}%0Apasscode%20{passcode}'.format(
         nomoriteung=config.nomor_iteung,
@@ -35,3 +36,15 @@ def replymsg(driver, data):
         passcode=passcode
     )
     return msgreply
+
+def hariSwitcher(hari):
+    switcher = {
+        'Monday': '1',
+        'Tuesday': '2',
+        'Wednesday': '3',
+        'Thursday': '4',
+        'Friday': '5',
+        'Saturday': '6',
+        'Sunday': '7',
+    }
+    return switcher.get(hari, 'days not found!')
