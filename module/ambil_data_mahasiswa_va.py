11c21c1a22e6635c32ed13693e398d9df326df13
dindamajesty13 <dindamesti@gmail.com>
2020-06-03 20:27:45 +0700

    oke

--- module/ambil_data_mahasiswa_va.py
+++ module/ambil_data_mahasiswa_va.py
@@ -21,7 +21,7 @@ def dbConnectVA():
 
 def getDataMahasiswaAktif():
     db=kelas.dbConnectSiap()
-    sql="select MhswID, Nama, EmailOrtu, HandphoneOrtu from simak_mst_mahasiswa where StatusMhswID='A' AND MhswID between 1133001 and 6194500"
+    sql="select MhswID, Nama, EmailOrtu, HandphoneOrtu, ProdiID from simak_mst_mahasiswa where StatusMhswID='A' AND MhswID between 1133001 and 6194500"
     with db:
         cur=db.cursor()
         cur.execute(sql)
@@ -33,6 +33,8 @@ def insertData(datamahasiswa):
     for i in MAHASISWAAKTIF:
         npm=i[0]
         nama=i[1]
+        prodiid=i[4]
+        angkatan=angkatanSwitcher(npm[1:3])
         if i[2] == None or i[2] == '' or i[2] == ' ' or i[2] == 'NULL':
             email='NULL'
         else:
@@ -42,18 +44,51 @@ def insertData(datamahasiswa):
         else:
             hp=i[3]
         try:
-            setData(npm, nama, email, hp)
+            setData(npm, nama, email, hp, prodiid, switcherJurusan(prodiid), angkatan)
         except:
             print('data sudah ada')
 
-def setData(npm, nama, email, handphone):
+def setData(npm, nama, email, handphone, prodiid, namaprodi, angkatan):
     db=dbConnectVA()
-    sql='INSERT INTO `db_ypbpi`.`VAMAHASISWA`(`MhswID`, `Nama`, `EmailOrtu`, `HandphoneOrtu`) VALUES ("{npm}", "{nama}", "{email}", "{handphone}")'.format(
+    sql='INSERT INTO `db_ypbpi`.`VAMAHASISWA`(`MhswID`, `Nama`, `EmailOrtu`, `HandphoneOrtu`, `ProdiID`, `NamaProdi`, `Angkatan`) VALUES ("{npm}", "{nama}", "{email}", "{handphone}", "{prodiid}", "{namaprodi}", "{angkatan}")'.format(
         npm=npm,
         nama=nama,
         email=email,
-        handphone=handphone
+        handphone=handphone,
+        prodiid=prodiid,
+        namaprodi=namaprodi,
+        angkatan=angkatan
     )
     with db:
         cur=db.cursor()
         cur.execute(sql)
+
+def angkatanSwitcher(npm):
+    switcher = {
+        '10': '2010',
+        '11': '2011',
+        '12': '2012',
+        '13': '2013',
+        '14': '2014',
+        '15': '2015',
+        '16': '2016',
+        '17': '2017',
+        '18': '2018',
+        '19': '2019',
+        '20': '2020',
+    }
+    return switcher.get(npm, 'notfound')
+
+def switcherJurusan(kode):
+    switcher = {
+        '13': 'D3 Teknik Informatika',
+        '14': 'D4 Teknik Informatika',
+        '23': 'D3 Manajemen Informatika',
+        '33': 'D3 Akuntansi',
+        '34': 'D4 Akuntansi',
+        '43': 'D3 Manajemen Bisnis',
+        '44': 'D4 Manajemen Bisnis',
+        '53': 'D3 Logistik Bisnis',
+        '54': 'D4 Logistik Bisnis',
+    }
+    return switcher.get(kode, "Not Found!")
