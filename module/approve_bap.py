ada6c80d8dd74812b9eb7dd73da45a2bf23df118
trianggadios <trianggadio@gmail.com>
2020-07-11 12:08:06 +0700

    fix code

--- module/approve_bap.py
+++ module/approve_bap.py
@@ -63,47 +63,44 @@ def replymsg(driver, data):
     wa.typeAndSendMessage(driver, wmsg)
     num=numbers.normalize(data[0])
     jadwalid=data[3].split(' ')[-1]
-    if kelas.getMkkode(jadwalid):
-        status=cekStatus(num)
-        updatefield=updateField(status)
-        if status == 'kaprodi':
-            kaprodiprodiid=getKaprodiProdiID(num)
-            if jadwalid == 'all':
-                statusbap=cek_tanda_tangan_bap.infoBAPKaprodi(kaprodiprodiid)[1]
-                if len(statusbap) > 0:
-                    for bapjadwalid in statusbap:
-                        confirmBKD(bapjadwalid, updatefield)
-                    msgreply=f'okee sudah berhasil #BOTNAME# approve semua datanya yaaa ini data yang berhasil #BOTNAME# approve\n\n*Jadwal ID* :\n'
-                    for i in statusbap:
-                        msgreply+=f'{i}\n'
-                else:
-                    msgreply='aduh wadidiw, ga ada nih berkas yang bisa di approve hihihihi'
+    status=cekStatus(num)
+    updatefield=updateField(status)
+    if status == 'kaprodi':
+        kaprodiprodiid=getKaprodiProdiID(num)
+        if jadwalid == 'all':
+            statusbap=cek_tanda_tangan_bap.infoBAPKaprodi(kaprodiprodiid)[1]
+            if len(statusbap) > 0:
+                for bapjadwalid in statusbap:
+                    confirmBKD(bapjadwalid, updatefield)
+                msgreply=f'okee sudah berhasil #BOTNAME# approve semua datanya yaaa ini data yang berhasil #BOTNAME# approve\n\n*Jadwal ID* :\n'
+                for i in statusbap:
+                    msgreply+=f'{i}\n'
             else:
-                if cekProdiIDKaproditoJadwalID(kaprodiprodiid, jadwalid):
-                    if cek_tanda_tangan_bap.cekMateriPerkuliahan(jadwalid):
-                        confirmBKD(jadwalid, updatefield)
-                        msgreply = f'okee sudah berhasil #BOTNAME# set approvalnya untuk JadwalID *{jadwalid}* yaa...'
-                    else:
-                        msgreply = f'waduh mohon maaf sepertinya belum bisa approve JadwalID yang {jadwalid} deh soalnya materinya belum lengkap atau masih ada yang kosong'
-                else:
-                    msgreply = f'hayooo Bapak/Ibu dari prodi mana hayooo kok mau set JadwalID yang lain hayoooo'
+                msgreply='aduh wadidiw, ga ada nih berkas yang bisa di approve hihihihi'
         else:
-            if jadwalid == 'all':
-                statusbap = cek_tanda_tangan_bap.infoBAPDeputi()[1]
-                if len(statusbap) > 0:
-                    for bapjadwalid in statusbap:
-                        confirmBKD(bapjadwalid, updatefield)
-                    msgreply = f'okee sudah berhasil #BOTNAME# approve semua datanya yaaa ini data yang berhasil #BOTNAME# approve\n\n*Jadwal ID* :\n'
-                    for i in statusbap:
-                        msgreply += f'{i}\n'
-                else:
-                    msgreply='yahhhh sayang sekali belum ada berkas yang siap untuk di approve nichhhhh'
-            else:
+            if cekProdiIDKaproditoJadwalID(kaprodiprodiid, jadwalid):
                 if cek_tanda_tangan_bap.cekMateriPerkuliahan(jadwalid):
                     confirmBKD(jadwalid, updatefield)
                     msgreply = f'okee sudah berhasil #BOTNAME# set approvalnya untuk JadwalID *{jadwalid}* yaa...'
                 else:
-                    msgreply = f'waduh mohon maaf sepertinya belum bisa approce JadwalID yang {jadwalid} deh soalnya materinya belum lengkap atau masih ada yang kosong'
+                    msgreply = f'waduh mohon maaf sepertinya belum bisa approve JadwalID yang {jadwalid} deh soalnya materinya belum lengkap atau masih ada yang kosong'
+            else:
+                msgreply = f'hayooo Bapak/Ibu dari prodi mana hayooo kok mau set JadwalID yang lain hayoooo'
     else:
-        msgreply=f'wah #BOTNAME# ga bisa nemuin JadwalID yang *{jadwalid}* deh coba cek dulu'
+        if jadwalid == 'all':
+            statusbap = cek_tanda_tangan_bap.infoBAPDeputi()[1]
+            if len(statusbap) > 0:
+                for bapjadwalid in statusbap:
+                    confirmBKD(bapjadwalid, updatefield)
+                msgreply = f'okee sudah berhasil #BOTNAME# approve semua datanya yaaa ini data yang berhasil #BOTNAME# approve\n\n*Jadwal ID* :\n'
+                for i in statusbap:
+                    msgreply += f'{i}\n'
+            else:
+                msgreply='yahhhh sayang sekali belum ada berkas yang siap untuk di approve nichhhhh'
+        else:
+            if cek_tanda_tangan_bap.cekMateriPerkuliahan(jadwalid):
+                confirmBKD(jadwalid, updatefield)
+                msgreply = f'okee sudah berhasil #BOTNAME# set approvalnya untuk JadwalID *{jadwalid}* yaa...'
+            else:
+                msgreply = f'waduh mohon maaf sepertinya belum bisa approce JadwalID yang {jadwalid} deh soalnya materinya belum lengkap atau masih ada yang kosong'
     return msgreply
