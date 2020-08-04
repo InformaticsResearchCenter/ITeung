b83a65035d93c161425dc477fa669768991d1e6e
trianggadios <trianggadio@gmail.com>
2020-08-04 15:08:15 +0700

    restore bap try and except

--- module/bkd.py
+++ module/bkd.py
@@ -849,14 +849,12 @@ def makePDFandSend(num):
                     siapayangkurang += ' | DEPUTI AKADEMIK'
                 kurangapprove += f'{config.whatsapp_api_lineBreak}{i[0]} | {kelasdetails[2]} | {kelas.toKelas(kelasdetails[-1])} {siapayangkurang}'
             msgkurang += f'{config.whatsapp_api_lineBreak}{config.whatsapp_api_lineBreak}wuowwww ada yang kurang nih ketika kamu request BAP, status approval nya masih belum di ACC nih dengan Jadwal ID:{kurangapprove}{config.whatsapp_api_lineBreak}'
-        print(msgkurang)
-        # wa.setOutbox(numbers.normalize(num), msgkurang)
+        wa.setOutbox(numbers.normalize(num), msgkurang)
     else:
-        print('kirim email')
-        # mail(getLecturerMail(lecturercode),
-        #      f'Halooooo, {config.bot_name} ngirim file nich....',
-        #      f'ini ya file Absensi BKD yang Bapak/Ibu minta silahkan di cek... ehee....',
-        #      getFilePath(getLecturerMail(lecturercode), 'bkd'))
+        mail(getLecturerMail(lecturercode),
+             f'Halooooo, {config.bot_name} ngirim file nich....',
+             f'ini ya file Absensi BKD yang Bapak/Ibu minta silahkan di cek... ehee....',
+             getFilePath(getLecturerMail(lecturercode), 'bkd'))
 
 def verifyDigitalSign(resultpasscode):
     kodedosen = resultpasscode.split(';')[1]
