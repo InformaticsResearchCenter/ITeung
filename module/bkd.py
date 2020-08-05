7baacaf8db28aef3f328fd1fdd83e45571e39d62
trianggadios <trianggadio@gmail.com>
2020-08-05 22:45:39 +0700

    fix bap

--- module/bkd.py
+++ module/bkd.py
@@ -336,6 +336,7 @@ def makePDFHeader():
 
 
 def makePDFFooter(matkuldetails, lecturercode, pdf):
+    print(matkuldetails)
     pdf.output('bkd/' + matkuldetails[1] + '-' + matkuldetails[2] + '-' + getLecturerMail(lecturercode) + '.pdf', 'F')
 
 
@@ -759,83 +760,89 @@ def makePDFandSend(num):
     mkkodes = getMkKode(lecturercode)
     for mkkode in mkkodes:
         jadwalids = getJadwalID(mkkode[0], lecturercode)
-        try:
-            pdf = makePDFHeader()
-            matkuldetailsfix = None
-            for jadwalid in jadwalids:
-                if getRencanaKehadiran(jadwalid[0]) == '0' or getProgramID(jadwalid[0]) == '.KER.':
-                    print('rencana kehadiran kurang dari 0 atau program kerja sama')
-                else:
-                    matkuldetails = kelas.getMkDetails(jadwalid[0])
-                    datamatkulbap = getBKDMatkul(jadwalid[0])
-                    semester = countSemester(jadwalid[0])
-                    sks = getSks(jadwalid[0])
-                    nama_kelas = kelas.toKelas(matkuldetails[6])
-                    kode_matkul = matkuldetails[1]
-                    nama_matkul = matkuldetails[2]
-                    datenow = datetime.date(datetime.now()).strftime('%d-%m-%Y')
-                    tanggalbap = datenow.split('-')[0]
-                    bulanbap = datenow.split('-')[1]
-                    tahunbap = datenow.split('-')[2]
-                    kode_nomor = f'ITeung/{tanggalbap}/{bulanbap}/{tahunbap}'
-                    tanggalpdfbap = f'{tanggalbap} {bulanSwitcher(bulanbap)} {tahunbap}'
-                    datafixbap = []
-                    datawkwkbap = ['Pertemuan', 'Tanggal', 'Materi Perkuliahan', 'Jam Mulai', 'Jam Selesai']
-                    datafixbap.append(datawkwkbap)
-                    for i in datamatkulbap:
-                        matkuldatalist = []
-                        matkuldatalist.append(i[0])
-                        matkuldatalist.append(i[1].strftime('%d-%m-%Y'))
-                        matkuldatalist.append(i[2])
-                        matkuldatalist.append(str(i[3]))
-                        matkuldatalist.append(str(i[4]))
-                        datafixbap.append(matkuldatalist)
-                    makePDFBAP(pdf, datafixbap, kode_nomor, tanggalpdfbap, kode_matkul, nama_matkul, nama_kelas, semester,
-                               sks)
-                    studentid, studentname = getandsetStudentIDandStudentNAME(jadwalid[0])
-                    presensidosens1 = getPresensiDosen(jadwalid[0], 0, 8)
-                    presensidosens2 = getPresensiDosen(jadwalid[0], 7, 15)
-                    for i in range(2):
-                        if i == 0:
-                            pertemuan = countPertemuan(presensidosens1)
-                            pdfpertemuan = ['1', '2', '3', '4', '5', '6', '7']
-                        else:
-                            pertemuan = countPertemuan(presensidosens2)
-                            pdfpertemuan = ['8', '9', '10', '11', '12', '13', '14']
-                        if len(pertemuan) < 7:
-                            rencanakehadiran=getRencanaKehadiran(jadwalid[0])
-                            kehadirannormal=14
-                            selisihkehadiran=kehadirannormal-int(rencanakehadiran)
-                            for i in range(selisihkehadiran):
-                                jumlahmahasiswa=len(pertemuan[0])
-                                data_pertemuan=[]
-                                for j in range(jumlahmahasiswa):
-                                    data_pertemuan.append('-')
-                                pertemuan.append(data_pertemuan)
-                        datas = list(zip(pertemuan[0], pertemuan[1], pertemuan[2], pertemuan[3], pertemuan[4], pertemuan[5],
-                                         pertemuan[6]))
-                        total = countTotal(datas)
-                        datas = list(
-                            zip(studentid, studentname, pertemuan[0], pertemuan[1], pertemuan[2], pertemuan[3],
-                                pertemuan[4],
-                                pertemuan[5], pertemuan[6]))
-                        number = countNumber(studentid)
-                        datapdf = list(
-                            zip(number, studentid, studentname, pertemuan[0], pertemuan[1], pertemuan[2], pertemuan[3],
-                                pertemuan[4], pertemuan[5], pertemuan[6], total))
-                        tanggal = tanggalBKDPresensi(getTanggalFromPresensiDosen(jadwalid[0]))
-                        datapdf.append(tanggal)
-                        statusapprove=cekStatusApproveBAP(jadwalid[0])
-                        makePDFInner(datapdf, matkuldetails, lecturername, pdf, pdfpertemuan, lecturercode, statusapprove)
-                        matkuldetailsfix = matkuldetails
-            makePDFFooter(matkuldetailsfix, lecturercode, pdf)
-        except Exception as e:
-            print(str(e))
-            print(f'pertemuan kurang dari {config.kehadiran}')
-            pertemuankurang.append(jadwalid[0])
+        # try:
+        pdf = makePDFHeader()
+        for jadwalid in jadwalids:
+            print(jadwalid)
+            matkuldetailsfix = kelas.getMkDetails(jadwalid[0])
+            if getRencanaKehadiran(jadwalid[0]) == '0':
+                print('rencana kehadiran kurang dari 0')
+            elif getProgramID(jadwalid[0]) == '.KER.':
+                print('program kerja sama')
+            else:
+                matkuldetails = kelas.getMkDetails(jadwalid[0])
+                datamatkulbap = getBKDMatkul(jadwalid[0])
+                semester = countSemester(jadwalid[0])
+                sks = getSks(jadwalid[0])
+                nama_kelas = kelas.toKelas(matkuldetails[6])
+                kode_matkul = matkuldetails[1]
+                nama_matkul = matkuldetails[2]
+                datenow = datetime.date(datetime.now()).strftime('%d-%m-%Y')
+                tanggalbap = datenow.split('-')[0]
+                bulanbap = datenow.split('-')[1]
+                tahunbap = datenow.split('-')[2]
+                kode_nomor = f'ITeung/{tanggalbap}/{bulanbap}/{tahunbap}'
+                tanggalpdfbap = f'{tanggalbap} {bulanSwitcher(bulanbap)} {tahunbap}'
+                datafixbap = []
+                datawkwkbap = ['Pertemuan', 'Tanggal', 'Materi Perkuliahan', 'Jam Mulai', 'Jam Selesai']
+                datafixbap.append(datawkwkbap)
+                for i in datamatkulbap:
+                    matkuldatalist = []
+                    matkuldatalist.append(i[0])
+                    matkuldatalist.append(i[1].strftime('%d-%m-%Y'))
+                    matkuldatalist.append(i[2])
+                    matkuldatalist.append(str(i[3]))
+                    matkuldatalist.append(str(i[4]))
+                    datafixbap.append(matkuldatalist)
+                makePDFBAP(pdf, datafixbap, kode_nomor, tanggalpdfbap, kode_matkul, nama_matkul, nama_kelas, semester,
+                           sks)
+                studentid, studentname = getandsetStudentIDandStudentNAME(jadwalid[0])
+                presensidosens1 = getPresensiDosen(jadwalid[0], 0, 8)
+                presensidosens2 = getPresensiDosen(jadwalid[0], 7, 15)
+                for i in range(2):
+                    if i == 0:
+                        pertemuan = countPertemuan(presensidosens1)
+                        pdfpertemuan = ['1', '2', '3', '4', '5', '6', '7']
+                    else:
+                        pertemuan = countPertemuan(presensidosens2)
+                        pdfpertemuan = ['8', '9', '10', '11', '12', '13', '14']
+                    if len(pertemuan) < 7:
+                        rencanakehadiran=getRencanaKehadiran(jadwalid[0])
+                        kehadirannormal=14
+                        selisihkehadiran=kehadirannormal-int(rencanakehadiran)
+                        for i in range(selisihkehadiran):
+                            jumlahmahasiswa=len(pertemuan[0])
+                            data_pertemuan=[]
+                            for j in range(jumlahmahasiswa):
+                                data_pertemuan.append('-')
+                            pertemuan.append(data_pertemuan)
+                    datas = list(zip(pertemuan[0], pertemuan[1], pertemuan[2], pertemuan[3], pertemuan[4], pertemuan[5],
+                                     pertemuan[6]))
+                    total = countTotal(datas)
+                    datas = list(
+                        zip(studentid, studentname, pertemuan[0], pertemuan[1], pertemuan[2], pertemuan[3],
+                            pertemuan[4],
+                            pertemuan[5], pertemuan[6]))
+                    number = countNumber(studentid)
+                    datapdf = list(
+                        zip(number, studentid, studentname, pertemuan[0], pertemuan[1], pertemuan[2], pertemuan[3],
+                            pertemuan[4], pertemuan[5], pertemuan[6], total))
+                    tanggal = tanggalBKDPresensi(getTanggalFromPresensiDosen(jadwalid[0]))
+                    datapdf.append(tanggal)
+                    statusapprove=cekStatusApproveBAP(jadwalid[0])
+                    makePDFInner(datapdf, matkuldetails, lecturername, pdf, pdfpertemuan, lecturercode, statusapprove)
+                    matkuldetailsfix = matkuldetails
+        makePDFFooter(matkuldetailsfix, lecturercode, pdf)
+        # except Exception as e:
+        #     print(str(e))
+        #     print(f'pertemuan kurang dari {config.kehadiran}')
+        #     pertemuankurang.append(jadwalid[0])
     cekkurangmateri = cekMateriByGrouping(lecturercode)
     cekkurangapproval = cekApprovalBAPByGrouping(lecturercode)
-    if cekkurangmateri[0] == False or cekkurangapproval[0] == False:
+    for i in cekkurangmateri[1]:
+        if len(cekkurangmateri[1]) == 1 and getProgramID(i[0]) == '.KER.':
+            cekkurangmateri=(True, [])
+    if len(pertemuankurang) > 0 or cekkurangmateri[0] == False or cekkurangapproval[0] == False:
         msgkurang=''
         if len(pertemuankurang) > 0:
             msgkurang+=f'hai haiii, kamu yang request BAP yaaa?{config.whatsapp_api_lineBreak}wahhh ada yang kurang nih pertemuannya ini Jadwal ID nya yaaa:'
@@ -859,7 +866,8 @@ def makePDFandSend(num):
                     siapayangkurang += ' | DEPUTI AKADEMIK'
                 kurangapprove += f'{config.whatsapp_api_lineBreak}{i[0]} | {kelasdetails[2]} | {kelas.toKelas(kelasdetails[-1])} {siapayangkurang}'
             msgkurang += f'{config.whatsapp_api_lineBreak}{config.whatsapp_api_lineBreak}wuowwww ada yang kurang nih ketika kamu request BAP, status approval nya masih belum di ACC nih dengan Jadwal ID:{kurangapprove}{config.whatsapp_api_lineBreak}'
-        wa.setOutbox(numbers.normalize(num), msgkurang)
+        # wa.setOutbox(numbers.normalize(num), msgkurang)
+        print(msgkurang)
     else:
         mail(getLecturerMail(lecturercode),
              f'Halooooo, {config.bot_name} ngirim file nich....',
