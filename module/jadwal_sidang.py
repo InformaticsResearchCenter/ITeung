91bf5eb4e22e050555c0a15ad1fb1bfb20844c4d
trianggadios <trianggadio@gmail.com>
2020-08-24 09:29:57 +0700

    jadwal sidang

---
+++ module/jadwal_sidang.py
@@ -0,0 +1,50 @@
+import pandas
+from module import kelas
+
+def auth(data):
+    if kelas.getKodeDosen(data[0]):
+        return True
+    else:
+        return False
+
+
+def replymsg(driver, data):
+    kode_dosen=kelas.getKodeDosen(data[0])
+    dosen_homebase=getHomebaseDosen(kode_dosen)
+    nama_file=f'jadwal_sidang_ta_{dosen_homebase}.xlsx'
+    status, df=openJadwalExcel(nama_file)
+    if status == False:
+        msgreply='mohon untuk memberikan jadwal sidang dalam bentuk excel ke admin'
+    else:
+        msgreply='ini dia jadwal sidangnya yaaa....\n\n'
+        for i, j in df.iterrows():
+            if j[0] == 'NN257L' or j[1] == 'NN257L' or j[2] == 'NN257L' or j[3] == 'NN257L':
+                msgreply+=f'PENGUJI UTAMA (1): {kelas.getNamaDosen(j[0])}\n' \
+                          f'PENGUJI PENDAMPING (2): {kelas.getNamaDosen(j[1])}\n' \
+                          f'PENGUJI UTAMA: {kelas.getNamaDosen(j[2])}\n' \
+                          f'PENGUJI PENDAMPING: {kelas.getNamaDosen(j[3])}\n' \
+                          f'NAMA MAHASISWA: {j[4]}\n' \
+                          f'JADWAL SIDANG: {j[5]}\n' \
+                          f'JAM SIDANG: {j[6]}\n\n'
+    return msgreply
+
+
+def getHomebaseDosen(dosenid):
+    db=kelas.dbConnectSiap()
+    sql=f'select Homebase from simak_mst_dosen where Login="{dosenid}"'
+    with db:
+        cur=db.cursor()
+        cur.execute(sql)
+        row=cur.fetchone()
+        if row is not None:
+            return row[0]
+        else:
+            return None
+
+
+def openJadwalExcel(namaFile):
+    try:
+        df=True, pandas.read_excel(f'{namaFile}')
+    except:
+        df=False, ''
+    return df
