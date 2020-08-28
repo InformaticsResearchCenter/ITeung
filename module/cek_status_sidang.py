65cda62dafe3af3c928cf33a004b498cee9d9a02
Diva Krishna <divakrishnam@gmail.com>
2020-08-28 11:42:56 +0800

    fix module cek status sidang and remove somefile

--- module/cek_status_sidang.py
+++ module/cek_status_sidang.py
@@ -38,45 +38,50 @@ def replymsg(driver, data):
 
 def checkSidang(npm, tahun_id):
     db=kelas.dbConnect()
-    msg = "*TTD BA & Pengesahan Sidang :*\n\n"
+    msg = "*TTD BA & Pengesahan Sidang :*\n"
     
     sql=f'select penguji_utama, penguji_pendamping, pembimbing_utama, pembimbing_pendamping, koordinator, kaprodi from sidang_data where npm="{npm}" and tahun_id="{tahun_id}"'
-    
+    # print(sql)
     with db:
         cur=db.cursor()
         cur.execute(sql)
         row=cur.fetchone()
-        if row[0]:
+        if row:
             if row[0]:
-                msg += f'Penguji Utama - Accept\n'
-            if row[1]:
-                msg += f'Penguji Pendamping - Accept\n'
-            if row[2]:
-                msg += f'Pembimbing Utama - Accept\n'
-            if row[3]:
-                msg += f'Pembimbing Pendamping - Accept\n'
-            if row[4]:
-                msg += f'Koordinator - Accept\n'
-            if row[5]:
-                msg += f'Kepala Prodi - Accept\n'                
+                if row[0]:
+                    msg += f'Penguji Utama - Accept\n'
+                if row[1]:
+                    msg += f'Penguji Pendamping - Accept\n'
+                if row[2]:
+                    msg += f'Pembimbing Utama - Accept\n'
+                if row[3]:
+                    msg += f'Pembimbing Pendamping - Accept\n'
+                if row[4]:
+                    msg += f'Koordinator - Accept\n'
+                if row[5]:
+                    msg += f'Kepala Prodi - Accept\n'                
+            else:
+                msg += "Belum Ada\n"
         else:
-            msg += "Belum Ada"
+            msg += "Belum Ada\n"
+    return msg
 
 def checkRevisi(npm, tahun_id):
     db=kelas.dbConnect()
-    msg = "*Revisi Sidang :*\n\n"
+    msg = "*Revisi Sidang :*\n"
     
     sql=f'select distinct penguji from revisi_data where npm="{npm}" and tahun_id="{tahun_id}"'
-    
+    # print(sql)
     with db:
         cur=db.cursor()
         cur.execute(sql)
         rows=cur.fetchall()
-        if rows is not None:
+        if rows:
             for row in rows:
                 msg += f'{row[0]} - Accept\n'
         else:
-            msg += "Belum Ada"
+            msg += "Belum Ada\n"
+    return msg
 
 def checkMhs(npm):
     db=kelas.dbConnect()
