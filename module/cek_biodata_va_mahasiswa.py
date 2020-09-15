24d0606153388ac939cea2869a98148c9911865d
trianggadios <trianggadio@gmail.com>
2020-09-15 19:58:30 +0700

    tambah data va

--- module/cek_biodata_va_mahasiswa.py
+++ module/cek_biodata_va_mahasiswa.py
@@ -12,7 +12,7 @@ def auth(data):
 
 def replymsg(driver, data):
     npm, nama_mahasiswa, prodi_id, phonenumber, email, penasehat_akademik=getDataMahasiswa(data[0])
-    virtual_account, customer_name, customer_email, customer_phone, trx_amount=getDataVaforMahasiswa(data[0])
+    virtual_account, customer_name, customer_email, customer_phone, trx_amount, trx_id=getDataVaforMahasiswa(data[0])
     ws=app.openfile().active
     prodi_singkatan = app.getProdiSingkatanFromProdiID(kelas.getProdiIDwithStudentID(npm)).lower()
     tingkat = f"tk{int(datetime.now().strftime('%Y')) - int(kelas.getTahunAngkatanWithStudentID(npm)) + 1}"
@@ -25,33 +25,56 @@ def replymsg(driver, data):
         tunggakan=float(0)
     app.openfile().close()
     ayah, ibu, handphoneortu=getNamaOrangTua(npm)
-    msgreply=f'*BIODATA MAHASISWA*\n' \
-             f'NPM: {npm}\n' \
-             f'Nama: {nama_mahasiswa}\n' \
-             f'Prodi: {kelas.getProdiNameWithStudentID(npm)}\n' \
-             f'Nomor Handphone: {phonenumber}\n' \
-             f'E-mail: {email}\n' \
-             f'Dosen Wali: {kelas.getNamaDosen(penasehat_akademik)}\n' \
-             f'Nama Orang Tua/Wali: {ayah} (Ayah) | {ibu} (Ibu)\n' \
-             f'No HP orang Tua/Wali: {handphoneortu}\n\n' \
-             f'*DATA VIRTUAL ACCOUNT BNI (Semester Ganjil 2020/2021)*\n' \
-             f'Virtual Account: {virtual_account}\n' \
-             f'Status Virtual Account: Aktif\n' \
-             f'Customer Name: {customer_name}\n' \
-             f'Customer Email: {customer_email}\n' \
-             f'Customer Phone Number: {customer_phone}\n' \
-             f'Biaya Paket SPP Per Semester: {app.floatToRupiah(float(biaya_pokok_spp))}\n' \
-             f'Biaya Tunggakan SPP: {app.floatToRupiah(tunggakan)}\n' \
-             f'Jumlah Tagihan: {app.floatToRupiah(float(trx_amount))}\n' \
-             f'Biaya Minimal Pembayaran: {app.floatToRupiah(float(trx_amount)/2)}\n' \
-             f'Batas KRS: 12 Oktober 2020 - 16 Oktober 2020\n\n'\
-             f'*CATATAN:* Untuk mempercepat layanan KRS Realtime *(langsung bayar langsung aktif dan bisa isi KRS)* anda diwajibkan melakukan pembayaran SPP menggunakan account VA anda, apabila pembayaran SPP tidak menggunakan account VA atau menggunakan metode transfer ke rekening YPBPI atau Giro Pos maka pengisian KRS dan aktivasi membutuhkan waktu 2 s.d 4 hari untuk mengecek bukti validasi pembayaran anda. Mohon kerjasamanya.'
+    payment_spp=getSPP(npm)
+    payment_toefl=getTOEFL(npm)
+    payment_ta=getTA(npm)
+    payment_sp=getSP(npm)
+    payment_ulang=getULANG(npm)
+    payment_wisuda=getWISUDA(npm)
+    msgreply = f'*BIODATA MAHASISWA*\n' \
+               f'NPM: {npm}\n' \
+               f'Nama: {nama_mahasiswa}\n' \
+               f'Prodi: {kelas.getProdiNameWithStudentID(npm)}\n' \
+               f'Nomor Handphone: {phonenumber}\n' \
+               f'E-mail: {email}\n' \
+               f'Dosen Wali: {kelas.getNamaDosen(penasehat_akademik)}\n' \
+               f'Nama Orang Tua/Wali: {ayah} (Ayah) | {ibu} (Ibu)\n' \
+               f'No HP orang Tua/Wali: {handphoneortu}\n\n'
+    if payment_spp:
+        msgreply+=f'*DATA VIRTUAL ACCOUNT BNI SPP (Semester Ganjil 2020/2021)*\n' \
+                  f'Kode Transaksi: {trx_id}\n' \
+                  f'Virtual Account: {virtual_account}\n' \
+                  f'Status Virtual Account: Aktif\n' \
+                  f'Customer Name: {customer_name}\n' \
+                  f'Customer Email: {customer_email}\n' \
+                  f'Customer Phone Number: {customer_phone}\n' \
+                  f'Biaya Paket SPP Per Semester: {app.floatToRupiah(float(biaya_pokok_spp))}\n' \
+                  f'Biaya Tunggakan SPP: {app.floatToRupiah(tunggakan)}\n' \
+                  f'Jumlah Tagihan: {app.floatToRupiah(float(trx_amount))}\n' \
+                  f'Biaya Minimal Pembayaran: {app.floatToRupiah(float(trx_amount) / 2)}\n' \
+                  f'Batas KRS: 12 Oktober 2020 - 16 Oktober 2020\n\n'
+    if payment_toefl:
+        msgreply+=f'*DATA VIRTUAL ACCOUNT BNI SPP (Semester Ganjil 2020/2021)*\n' \
+                  f'Kode Transaksi: {payment_toefl["trx_id"]}\n\n'
+    if payment_ta:
+        msgreply+=f'*DATA VIRTUAL ACCOUNT BNI TA (Semester Ganjil 2020/2021)*\n' \
+                  f'Kode Transaksi: {payment_ta["trx_id"]}\n\n'
+    if payment_sp:
+        msgreply += f'*DATA VIRTUAL ACCOUNT BNI SPP (Semester Ganjil 2020/2021)*\n' \
+                    f'Kode Transaksi: {payment_sp["trx_id"]}\n\n'
+    if payment_ulang:
+        msgreply += f'*DATA VIRTUAL ACCOUNT BNI SPP (Semester Ganjil 2020/2021)*\n' \
+                    f'Kode Transaksi: {payment_ulang["trx_id"]}\n\n'
+    if payment_wisuda:
+        msgreply += f'*DATA VIRTUAL ACCOUNT BNI SPP (Semester Ganjil 2020/2021)*\n' \
+                    f'Kode Transaksi: {payment_wisuda["trx_id"]}\n\n'
+    msgreply+=f'*CATATAN:* Untuk mempercepat layanan KRS Realtime *(langsung bayar langsung aktif dan bisa isi KRS)* anda diwajibkan melakukan pembayaran SPP menggunakan account VA anda, apabila pembayaran SPP tidak menggunakan account VA atau menggunakan metode transfer ke rekening YPBPI atau Giro Pos maka pengisian KRS dan aktivasi membutuhkan waktu 2 s.d 4 hari untuk mengecek bukti validasi pembayaran anda. Mohon kerjasamanya.'
     return msgreply
 
 def getDataVaforMahasiswa(phonenumber):
     phonenumber = numbers.normalize(phonenumber)
     db=va_parent.dbConnectVA()
-    sql=f"select virtual_account, customer_name, customer_email, customer_phone, trx_amount from upload where customer_phone='{phonenumber}' ORDER BY upload_id DESC LIMIT 1"
+    sql=f"select virtual_account, customer_name, customer_email, customer_phone, trx_amount, trx_id from upload where customer_phone='{phonenumber}' ORDER BY upload_id DESC LIMIT 1"
     with db:
         cur=db.cursor()
         cur.execute(sql)
@@ -81,3 +104,87 @@ def getNamaOrangTua(npm):
         cur=db.cursor()
         cur.execute(sql)
         return cur.fetchone()
+
+def getSPP(npm):
+    db=va_parent.dbConnectVA()
+    sql=f"SELECT * FROM `upload` where trx_id like '%SPP%{npm}%' order by upload_id desc limit 1"
+    with db:
+        cur=db.cursor()
+        cur.execute(sql)
+        data=cur.fetchone()
+        if data != None:
+            fields = map(lambda x: x[0], cur.description)
+            result = dict(zip(fields, data))
+        else:
+            result = None
+        return result
+
+def getTOEFL(npm):
+    db=va_parent.dbConnectVA()
+    sql=f"SELECT * FROM `upload` where trx_id like '%TOEFL%{npm}%' order by upload_id desc limit 1"
+    with db:
+        cur=db.cursor()
+        cur.execute(sql)
+        data=cur.fetchone()
+        if data != None:
+            fields = map(lambda x: x[0], cur.description)
+            result = dict(zip(fields, data))
+        else:
+            result = None
+        return result
+
+def getSP(npm):
+    db=va_parent.dbConnectVA()
+    sql=f"SELECT * FROM `upload` where trx_id like '%SP%{npm}%' order by upload_id desc limit 1"
+    with db:
+        cur=db.cursor()
+        cur.execute(sql)
+        data=cur.fetchone()
+        if data != None:
+            fields = map(lambda x: x[0], cur.description)
+            result = dict(zip(fields, data))
+        else:
+            result = None
+        return result
+
+def getULANG(npm):
+    db=va_parent.dbConnectVA()
+    sql=f"SELECT * FROM `upload` where trx_id like '%ULANG%{npm}%' order by upload_id desc limit 1"
+    with db:
+        cur=db.cursor()
+        cur.execute(sql)
+        data=cur.fetchone()
+        if data != None:
+            fields = map(lambda x: x[0], cur.description)
+            result = dict(zip(fields, data))
+        else:
+            result = None
+        return result
+
+def getWISUDA(npm):
+    db=va_parent.dbConnectVA()
+    sql=f"SELECT * FROM `upload` where trx_id like '%SP%{npm}%' order by upload_id desc limit 1"
+    with db:
+        cur=db.cursor()
+        cur.execute(sql)
+        data=cur.fetchone()
+        if data != None:
+            fields = map(lambda x: x[0], cur.description)
+            result = dict(zip(fields, data))
+        else:
+            result = None
+        return result
+
+def getTA(npm):
+    db=va_parent.dbConnectVA()
+    sql=f"SELECT * FROM `upload` where trx_id like '%TA%{npm}%' order by upload_id desc limit 1"
+    with db:
+        cur=db.cursor()
+        cur.execute(sql)
+        data=cur.fetchone()
+        if data != None:
+            fields = map(lambda x: x[0], cur.description)
+            result = dict(zip(fields, data))
+        else:
+            result = None
+        return result
