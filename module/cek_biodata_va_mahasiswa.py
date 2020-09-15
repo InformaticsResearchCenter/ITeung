4a0b63d27c798a823fc927a8dca1078bc1a93556
trianggadios <trianggadio@gmail.com>
2020-09-15 20:18:37 +0700

    fix

--- module/cek_biodata_va_mahasiswa.py
+++ module/cek_biodata_va_mahasiswa.py
@@ -12,25 +12,27 @@ def auth(data):
 
 def replymsg(driver, data):
     npm, nama_mahasiswa, prodi_id, phonenumber, email, penasehat_akademik=getDataMahasiswa(data[0])
-    virtual_account, customer_name, customer_email, customer_phone, trx_amount, trx_id=getDataVaforMahasiswa(data[0])
     ws=app.openfile().active
     prodi_singkatan = app.getProdiSingkatanFromProdiID(kelas.getProdiIDwithStudentID(npm)).lower()
     tingkat = f"tk{int(datetime.now().strftime('%Y')) - int(kelas.getTahunAngkatanWithStudentID(npm)) + 1}"
     angkatan = kelas.getTahunAngkatanWithStudentID(npm)
     key = f'{prodi_singkatan}{tingkat}{angkatan}'
     biaya_pokok_spp = app.getDataDefault(key, ws)
-    if trx_amount > biaya_pokok_spp:
-        tunggakan=float(int(trx_amount)-int(biaya_pokok_spp))
+
+    payment_spp = getSPP(npm)
+    payment_toefl = getTOEFL(npm)
+    payment_ta = getTA(npm)
+    payment_sp = getSP(npm)
+    payment_ulang = getULANG(npm)
+    payment_wisuda = getWISUDA(npm)
+
+    if payment_spp['trx_amount'] > biaya_pokok_spp:
+        tunggakan=float(int(payment_spp['trx_amount'])-int(biaya_pokok_spp))
     else:
         tunggakan=float(0)
+        
     app.openfile().close()
     ayah, ibu, handphoneortu=getNamaOrangTua(npm)
-    payment_spp=getSPP(npm)
-    payment_toefl=getTOEFL(npm)
-    payment_ta=getTA(npm)
-    payment_sp=getSP(npm)
-    payment_ulang=getULANG(npm)
-    payment_wisuda=getWISUDA(npm)
     msgreply = f'*BIODATA MAHASISWA*\n' \
                f'NPM: {npm}\n' \
                f'Nama: {nama_mahasiswa}\n' \
@@ -42,12 +44,12 @@ def replymsg(driver, data):
                f'No HP orang Tua/Wali: {handphoneortu}\n\n'
     if payment_spp:
         msgreply+=f'*DATA VIRTUAL ACCOUNT BNI SPP (Semester Ganjil 2020/2021)*\n' \
-                  f'Kode Transaksi: {trx_id}\n' \
-                  f'Virtual Account: {virtual_account}\n' \
+                  f'Kode Transaksi: {payment_spp["trx_id"]}\n' \
+                  f'Virtual Account: {payment_spp["virtual_account"]}\n' \
                   f'Status Virtual Account: Aktif\n' \
-                  f'Customer Name: {customer_name}\n' \
-                  f'Customer Email: {customer_email}\n' \
-                  f'Customer Phone Number: {customer_phone}\n' \
+                  f'Customer Name: {payment_spp["customer_name"]}\n' \
+                  f'Customer Email: {payment_spp["customer_email"]}\n' \
+                  f'Customer Phone Number: {payment_spp["customer_phone"]}\n' \
                   f'Biaya Paket SPP Per Semester: {app.floatToRupiah(float(biaya_pokok_spp))}\n' \
                   f'Biaya Tunggakan SPP: {app.floatToRupiah(tunggakan)}\n' \
                   f'Jumlah Tagihan: {app.floatToRupiah(float(trx_amount))}\n' \
