a3c9fa4b75688dc0af74f735722f3dc10e29dd62
trianggadios <trianggadio@gmail.com>
2020-10-10 10:44:03 +0700

    absensi cb

---
+++ module/absensi_cb_mulai.py
@@ -0,0 +1,33 @@
+from lib import numbers
+
+import config
+
+def auth(data):
+    if numbers.normalize(data[0]) in config.nomor_koor_pleton_cb:
+        return True
+    else:
+        return False
+
+def replymsg(driver, data):
+    grp=data[1]
+    kode_pleton=grp.split('-')[1]
+    nama_pleton=pletonSwitcher(kode_pleton)
+    msgreply=f'okeeeeee, absensi CB untuk *PLETON {nama_pleton}* dimulai, kepada peserta Character Building Politeknik Pos Indonesia 2020 untuk segera absen ke ITeung yaa, cukup dengan kirimkan pesannya ke GROUP PLETON kamu sudah bisa terabsen, minimal 2 - 3 pesan yaa...'
+    return msgreply
+
+def pletonSwitcher(kode_pleton):
+    switcher = {
+        '1': 'PLETON 1',
+        '2': 'PLETON 2',
+        '3': 'PLETON 3',
+        '4': 'PLETON 4',
+        '5': 'PLETON 5',
+        '6': 'PLETON 6',
+        '7': 'PLETON 7',
+        '8': 'PLETON 8',
+        '9': 'PLETON 9',
+        '10': 'PLETON 10',
+        '11': 'PLETON 11',
+        '12': 'PLETON 12',
+    }
+    return switcher.get(kode_pleton, None)
