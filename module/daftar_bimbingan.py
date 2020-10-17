79a0c6a44979788de9e7eabc8105504ca85f381b
trianggadios <trianggadio@gmail.com>
2020-10-17 16:21:48 +0700

    add tipe bimbingna

--- module/daftar_bimbingan.py
+++ module/daftar_bimbingan.py
@@ -17,7 +17,8 @@ def replymsg(driver, data):
     try:
         pembimbing_1 = msg.split(' pembimbing 1 ')[1].split(' pembimbing 2 ')[0]
         pembimbing_2 = msg.split(' pembimbing 2 ')[1].split(' judul ')[0]
-        judul = data[3].split(' judul ')[1]
+        tipe_bimbingan= msg.split(' tipe bimbingan ')[1]
+        judul = data[3].split(' judul ')[1].split(' tipe bimbingan ')[0]
     except:
         return f'aduuuu salah keyword nih bro bro bri brii.... coba dicek lagi yaa keywordnyaa....'
     if cekBimbinganData(npm):
@@ -25,7 +26,8 @@ def replymsg(driver, data):
             npm,
             pembimbing_1.upper(),
             pembimbing_2.upper(),
-            judul
+            judul,
+            tipe_bimbingan
         )
         return f'okeee sudah {config.bot_name} update menjadi\n\nNPM: {npm}\nNama: {nama}\nPembimbing 1: {kelas.getNamaDosen(pembimbing_1)}\nPembimbing 2: {"-" if pembimbing_2 == "-" else kelas.getNamaDosen(pembimbing_2)}\nJudul: {judul}'
     else:
@@ -33,20 +35,21 @@ def replymsg(driver, data):
             npm,
             pembimbing_1.upper(),
             pembimbing_2.upper(),
-            judul
+            judul,
+            tipe_bimbingan
         )
         return f'okeee sudah {config.bot_name} masukin yaaa datanya\n\nNPM: {npm}\nNama: {nama}\nPembimbing 1: {kelas.getNamaDosen(pembimbing_1)}\nPembimbing 2: {"-" if pembimbing_2 == "-" else kelas.getNamaDosen(pembimbing_2)}\nJudul: {judul}'
 
-def insertNewBimbinganData(npm, pembimbing1, pembimbing2, judul):
+def insertNewBimbinganData(npm, pembimbing1, pembimbing2, judul, tipe_bimbingan):
     db=kelas.dbConnect()
-    sql=f"INSERT INTO `wanda`.`bimbingan_data`(`npm`, `pembimbing1`, `pembimbing2`, `tahun_id`, `approval_pembimbing1`, `approval_pembimbing2`, `approval_koordinator1`, `approval_koordinator2`, `judul`) VALUES ('{npm}', '{pembimbing1}', '{pembimbing2}', '{kelas.getTahunID()}', 'false', 'false', 'false', 'false', '{judul}');"
+    sql=f"INSERT INTO `wanda`.`bimbingan_data`(`npm`, `pembimbing1`, `pembimbing2`, `tahun_id`, `approval_pembimbing1`, `approval_pembimbing2`, `approval_koordinator1`, `approval_koordinator2`, `judul`, `tipe_bimbingan`) VALUES ('{npm}', '{pembimbing1}', '{pembimbing2}', '{kelas.getTahunID()}', 'false', 'false', 'false', 'false', '{judul}', '{tipe_bimbingan}');"
     with db:
         cur=db.cursor()
         cur.execute(sql)
 
-def updateBimbinganData(npm, pembimbing1, pembimbing2, judul, tahun_id=kelas.getTahunID()):
+def updateBimbinganData(npm, pembimbing1, pembimbing2, judul, tipe_bimbingan, tahun_id=kelas.getTahunID()):
     db=kelas.dbConnect()
-    sql=f"UPDATE `wanda`.`bimbingan_data` SET `pembimbing1` = '{pembimbing1}', `pembimbing2` = '{pembimbing2}', `judul` = '{judul}' WHERE `npm` = '{npm}' and tahun_id = '{tahun_id}';"
+    sql=f"UPDATE `wanda`.`bimbingan_data` SET `pembimbing1` = '{pembimbing1}', `pembimbing2` = '{pembimbing2}', `judul` = '{judul}', `tipe_bimbingan` = '{tipe_bimbingan}' WHERE `npm` = '{npm}' and tahun_id = '{tahun_id}';"
     with db:
         cur=db.cursor()
         cur.execute(sql)
