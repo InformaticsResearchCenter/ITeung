962df0d505c900215648c817f4c888465d30c94e
trianggadios <trianggadio@gmail.com>
2020-10-29 21:13:30 +0700

    change query for specified class

--- module/perwalian_mulai.py
+++ module/perwalian_mulai.py
@@ -13,14 +13,28 @@ def replymsg(driver, data):
     tahun_angkatan=f'{data[1].split("-")[3]}1'
     msgreply=f'haiyooooo mari mari kita laksanakan perwalian bersama Bapak/Ibu dosen kita tercintaaa....\nBerikut ITeung berikan untuk peserta absensi Perwaliannya yaaa:\n\n'
     nomor_urut=1
-    for data in getPesertaPerwalian(kode_dosen, tahun_angkatan):
+    kelas_number=f'{kelas.kodeKelas(data[1].split("-")[2])}'
+    for data in getPesertaPerwalian(kode_dosen, tahun_angkatan, kelas_number):
         msgreply+=f'{nomor_urut}. *{data["MhswID"]}* - *{data["Nama"]}*\n'
         nomor_urut+=1
     return msgreply
 
-def getPesertaPerwalian(kode_dosen, tahun_angkatan):
+def getPesertaPerwalian(kode_dosen, tahun_angkatan, kelas_number):
     db=kelas.dbConnectSiap()
-    sql=f'select * from simak_mst_mahasiswa where PenasehatAkademik="{kode_dosen}" and TahunID={tahun_angkatan}'
+    sql=f'select mst.MhswID,mst.Nama,mst.PenasehatAkademik,mst.TahunID,krs.Kelas ' \
+        f'from ' \
+        f'simak_mst_mahasiswa as mst ' \
+        f'join ' \
+        f'simak_trn_krs as krs ' \
+        f'where mst.MhswID=krs.MhswID ' \
+        f'and ' \
+        f'mst.TahunID={tahun_angkatan} ' \
+        f'and ' \
+        f'krs.kelas="{kelas_number}" ' \
+        f'and ' \
+        f'mst.PenasehatAkademik="{kode_dosen}" ' \
+        f'group by ' \
+        f'mst.MhswID;'
     with db:
         cur=db.cursor()
         cur.execute(sql)
