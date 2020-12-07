a604317bec9ae4aacb52a810907c120710af886e
Tri Angga Dio Simamora <trianggadio@gmail.com>
2020-12-07 12:10:21 +0700

    bap pertemuan ranger

--- module/bkd.py
+++ module/bkd.py
@@ -825,6 +825,12 @@ def makePDFandSend(num):
                     studentid, studentname = getandsetStudentIDandStudentNAME(jadwalid[0])
                     presensidosens1 = getPresensiDosen(jadwalid[0], 0, 8)
                     presensidosens2 = getPresensiDosen(jadwalid[0], 7, 15)
+                    
+                    if presensidosens2 == ():
+                        ranger = 1
+                    else:
+                        ranger = 2
+                    
                     for i in range(2):
                         if i == 0:
                             pertemuan = countPertemuan(presensidosens1)
